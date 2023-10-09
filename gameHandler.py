7f0f1e63173af58e57e1b657add6893f3db74933
jakeacoc <jakeacoc@buffalo.edu>
2023-10-09 18:10:26 -0400

    ChangedToSprite
    Changed Player Class to use sprite based image

--- gameHandler.py
+++ gameHandler.py
@@ -19,53 +19,132 @@ PLAYER_VEL=4 #Player Movement speed
 
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
+def flip_image(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
 
-class Player(pygame.sprite.Sprite):
-    COLOR=(0,255,0)
-    def __init__(self, x, y, w, h):
-        self.rect = pygame.Rect(x,y,w,h)
-        self.x_pos=x
-        self.y_pos=y
-        self.inair=False
-        self.x_vel=0
-        self.y_vel=0
-        self.air_time=0
-        self.mask=None
+def load_sprite_sheets(directory1, directory2, width, height, direction=False):
+    path = join("assets", directory1, directory2)
+    images = [f for f in listdir(path) if isfile(join(path,f))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+        
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            # scales all sprite sheets up double size (including player)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip_image(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+
+    return all_sprites
+
+class Player(pygame.sprite.Sprite):  
+
+    # would actually read this value from file/however we end up doing it
+    SPRITES = load_sprite_sheets("Characters", "Celia", 32, 32, True)
+    ANIMATION_DELAY = 3
+    GRAVITY = 1
+
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_velocity, self.y_velocity = 0, 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+        self.fall_count = 0
+        self.jump_count = 0
+        self.hit = False
+        self.hit_count = 0
 
     def move(self, dx, dy):
-        self.rect.x +=dx
+        self.rect.x += dx
         self.rect.y += dy
 
-    def move_left(self, vel):
-        self.x_vel= -vel
-        #Futue: Handle direction change
-    def move_right(self, vel):
-        self.x_vel= vel
-        #Future: Handle direction change
+    def move_left(self, velocity):
+        self.x_velocity = -velocity
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
 
-    def loop(self, fps):
-        self.y_vel+=min(1,(self.air_time/fps)*GRAVITY)
-        self.move(self.x_vel,self.y_vel)
-        self.air_time+=1
+    def move_right(self, velocity):
+        self.x_velocity = velocity
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+
+    # does allow double jump
+    def jump(self):
+        self.y_velocity = -self.GRAVITY * 8
+        self.animation_count = 0
+        self.jump_count += 1
+        if self.jump_count == 1:
+            self.fall_count = 0
 
     def landed(self):
-        self.air_time = 0
-        self.y_vel = 0
-        self.inair=False
+        self.fall_count = 0
+        self.y_velocity = 0
+        self.jump_count = 0
 
     def hit_head(self):
+        # fall_count?
         self.count = 0
-        self.y_vel = 0
+        self.y_velocity *= -1
 
-    def jump(self):
-        self.y_vel=-GRAVITY*10
-        self.inair=True
+    def make_hit(self):
+        self.hit = True
+        self.hit_count = 0
+
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.hit:
+            sprite_sheet = "hit"
+        if self.y_velocity < 0:
+            if self.jump_count == 1:
+                sprite_sheet = "jump"
+            elif self.jump_count == 2:
+                sprite_sheet = "double_jump"
+        elif self.y_velocity > self.GRAVITY * 2:
+            sprite_sheet = "fall"
+        elif self.x_velocity != 0:
+            sprite_sheet = "run"
+        
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+        self.update()
 
     def update(self):
-        x=0
-        #Need Sprites
-    def draw(self, win):
-        pygame.draw.rect(win,self.COLOR, self.rect)
+        self.rect = self.sprite.get_rect(topleft=(self.rect.x, self.rect.y))
+        self.mask = pygame.mask.from_surface(self.sprite)
+    
+    def loop(self, fps):
+        # gravity
+        self.y_velocity += min(1, (self.fall_count / fps) * self.GRAVITY)
+        self.move(self.x_velocity, self.y_velocity)
+
+        if self.hit:
+            self.hit_count += 1
+        if self.hit_count > fps * 2:
+            self.hit = False
+            self.hit_count = 0
+
+        self.fall_count += 1
+        self.update_sprite()
+ 
+    def draw(self, window, offset_x):
+        window.blit(self.sprite, (self.rect.x - offset_x, self.rect.y))
 
 def get_background(name):
     image = pygame.image.load(join("Backgrounds",name))
