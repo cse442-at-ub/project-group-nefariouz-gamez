f157a5c801ab4cd31f9efcb872fd35f5d547fede
Jordan <jmcentee1410@gmail.com>
2023-10-10 12:46:05 -0400

    Added buttons from button class to settings page

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 import pygame
+from Button import *
 
 pygame.init()
 
@@ -11,6 +12,11 @@ def display_settings_page(screen):
     background_img = pygame.image.load("images/plain_background.png")
     background_img = pygame.transform.scale(background_img, (screen_width, screen_height))
 
+    # initializes buttons, button dimensions: 300x54, buttons have a 70 unit gap between them
+    tutorial_btn = Button(screen_width/2, (screen_height/2)+50, "TUTORIAL")
+    character_btn = Button(screen_width/2, (screen_height/2)+120, "CHOOSE CHARACTER")
+    return_btn = Button(screen_width/2, (screen_height/2)+190, "RETURN TO MAIN")
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -18,6 +24,9 @@ def display_settings_page(screen):
                 running = False
 
         screen.blit(background_img, (0, 0))
+        tutorial_btn.draw(screen)
+        character_btn.draw(screen)
+        return_btn.draw(screen)
 
         pygame.display.flip()
 
