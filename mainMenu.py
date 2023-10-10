cb54bb2685aaa6854990de1bf858aa85d43088ec
Jordan <jmcentee1410@gmail.com>
2023-10-09 20:01:21 -0400

    Added background image to game window

--- mainMenu.py
+++ mainMenu.py
@@ -4,21 +4,21 @@ pygame.init()
 
 def display_main_menu(screen):
 
-    background_color = (250, 200, 250)
+    background_img = pygame.image.load("images/main_background.png")
+    background_img = pygame.transform.scale(background_img, (screen.get_size()[0], screen.get_size()[1]))
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
-        screen.fill(background_color)
+                
+        screen.blit(background_img, (0,0))
 
         pygame.display.flip()
 
-pygame.quit()
+    pygame.quit()
     
-
 if __name__ == "__main__":
     screen = pygame.display.set_mode((1248, 702), pygame.RESIZABLE)
     pygame.display.set_caption("Shrubbery Quest")
