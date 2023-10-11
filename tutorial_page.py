cab5216795683cfd5d511994aa8e0c37264bba43
Blake <75831333+BlakeDase2@users.noreply.github.com>
2023-10-11 00:30:05 -0400

    Changed Tutorial page to use draw_background and draw_button.

--- tutorial_page.py
+++ tutorial_page.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+from draw_background import draw_background
+from draw_button import draw_button
 
 
 # Call show_tutorial with screen (pygame.display.set_mode(screen_size))
@@ -10,16 +12,14 @@ def show_tutorial(screen):
 
     # Run game loop for this page
     while True:
-        # Draw background
         screen_size = screen.get_size()
-        c_background_img = pygame.transform.scale(background_img, screen_size)
-        screen.blit(c_background_img, (0, 0))
+        button_size = (screen_size[0] / 4.286, screen_size[1] / 16)
+
+        # Draw background
+        draw_background(screen, background_img, screen_size)
 
         # Draw return button
-        c_return_button_img = pygame.transform.scale(return_button_img,(screen_size[0]/4.2, screen_size[1]/14.5))
-        return_button_rect = c_return_button_img.get_rect()
-        return_button_rect.topleft = (screen_size[0]/30, screen_size[1]/36.4)
-        screen.blit(c_return_button_img, return_button_rect.topleft)
+        return_button_rect = draw_button(screen, return_button_img, button_size, (screen_size[0]/27.907, screen_size[1]/36.364))
 
         # Check inputs
         for event in pygame.event.get():
