4eb286f0dc6a84d7dfdb76b84820462871f07b9c
Blake <75831333+BlakeDase2@users.noreply.github.com>
2023-10-13 15:17:38 -0400

    Removed unneeded files.

--- draw_background.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-
-# Call to draw the background for your page
-def draw_background(screen, background_img, screen_size):
-    background_img = pygame.transform.scale(background_img, screen_size)
-    screen.blit(background_img, (0, 0))
